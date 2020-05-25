8481acbba1c4d7c356de8c137d0ceb2c6891e9cd
MrWindwaker <lagr45@gmail.com>
2020-05-24 21:30:53 -0500

    I dont know

--- learning_users/urls.py
+++ learning_users/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
+admin.autodiscover()
+
 from django.urls import path, include
 from basic_app import views
 
@@ -23,5 +26,5 @@ urlpatterns = [
     path('basic_app/', include('basic_app.urls')),
     path('logout/', views.user_logout, name = 'logout'),
     path('special/', views.special, name = 'special'),
-    
+
 ]
